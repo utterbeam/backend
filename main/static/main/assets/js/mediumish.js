8dd8f80576777c515ba3ee9511f7c174edd78db4
Vishrut Kohli <vishrutkohli@Vishruts-MacBook-Air.local>
2018-04-07 04:22:07 +0530

    template improved

--- main/static/main/assets/js/mediumish.js
+++ main/static/main/assets/js/mediumish.js
@@ -9,3 +9,16 @@ $(function(){
       }
   });
 });
+
+
+
+$('.btn-search').click(function(){
+  $('.searchbar').toggleClass('clicked');
+  $('.stage').toggleClass('faded');
+
+  
+  if($('.searchbar').hasClass('clicked')){
+    $('.btn-extended').focus();
+  }
+  
+});
