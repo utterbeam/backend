507617a14df9828a4d81a63afdeba0b9b580d5c0
shivam kohli <kohlishivam5522@gmail.com>
2018-04-09 04:45:25 +0530

    added new template

--- main/views.py
+++ main/views.py
@@ -4,10 +4,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-	return render(request,'main/index.html')
+	return render(request,'main/newTemplate/index.html')
 
 def author(request):
-	return render(request,'main/author.html')
+	return render(request,'main/newTemplate/author/iherzog/index.html')
 
 def post(request):
 	return render(request,'main/post.html')
