ca8f3b7a2db7294adb51cfff898b68b0f548e571
shivam kohli <kohlishivam5522@gmail.com>
2018-04-12 12:32:18 +0530

    adding basic database

--- main/views.py
+++ main/views.py
@@ -19,4 +19,12 @@ def upload(request):
 	return render(request,'main/upload.html')
 
 def index2(request):
-	return render(request,'main/newTemplate/index2.html')
+	context_dict = {}
+	context_dict['backgroundThumb'] = "http://kohette.com/wpthemes/narratium/wp-content/uploads/2017/10/Victo-Ngai-226x300.jpg"
+	context_dict['backgroundLarge'] = "http://kohette.com/wpthemes/narratium/wp-content/uploads/2017/10/Victo-Ngai-772x1024.jpg"
+	context_dict['heading'] = "The biggest lie about the future is that it's going to look much different from today."
+	context_dict['subText'] = "What the Near Future Is Actually Going to Look Like."
+	author = ['Alex' , 'Zack']
+	context_dict['author'] = author
+	print context_dict
+	return render(request,'main/newTemplate/index2.html',context_dict)
