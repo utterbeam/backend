783b76afe2f3f784da2bc7e61891ea4bd6468847
Vishrut Kohli <vishrutkohli@Vishruts-MacBook-Air.local>
2018-04-23 05:56:21 +0530

    facebook login step 1 check

--- main/views.py
+++ main/views.py
@@ -115,7 +115,7 @@ def facebook_login(request):
 	# https://www.facebook.com/v2.12/dialog/oauth?client_id=190043084948279&redirect_uri=https://utterbeam.herokuapp.com/login&state="{st=state123abc,ds=123456789}"
 
 	code = request.GET.get('code')
-	print code
+	# print code
 
 
 	return HttpResponse(str(code))
