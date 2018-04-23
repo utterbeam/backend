1f8918dc833f417d532d2ed90cf4f35bd0aea077
shivam kohli <kohlishivam5522@gmail.com>
2018-04-24 01:36:29 +0530

    changed database to postgresql

--- uttterbeam/settings.py
+++ uttterbeam/settings.py
@@ -89,8 +89,7 @@ DATABASES = {
 
 
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 
