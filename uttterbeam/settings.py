132c6c99de0bbf1231699054983eeb243e5ecf3b
Vishrut Kohli <vishrutkohli@Vishruts-MacBook-Air.local>
2018-04-24 01:57:04 +0530

    a

--- uttterbeam/settings.py
+++ uttterbeam/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
 
 
 import dj_database_url
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 
